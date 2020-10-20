8f041393b4eea1a9541d254004c9c8c067c71424
Tamas Jozsa <tjozsa@gmail.com>
2020-10-20 05:11:51 +0200

    Update BrowserFactory.java

--- src/test/java/org/seleniumdemo/common/BrowserFactory.java
+++ src/test/java/org/seleniumdemo/common/BrowserFactory.java
@@ -5,15 +5,17 @@ import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
+import static io.github.bonigarcia.wdm.DriverManagerType.FIREFOX;
 
 class BrowserFactory {
     static WebDriver getBrowser(String browserName) {
         WebDriver driver;
         if (browserName != null && browserName.equals("chrome")) {
-            ChromeDriverManager.getInstance().setup();
+            ChromeDriverManager.getInstance(CHROME).setup();
             driver = new ChromeDriver();
         } else {
-            FirefoxDriverManager.getInstance().setup();
+            FirefoxDriverManager.getInstance(FIREFOX).setup();
             driver = new FirefoxDriver();
         }
         return driver;
